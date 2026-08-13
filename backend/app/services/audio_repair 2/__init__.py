2b985a74ba079eb36eb10753c600edf215459199
Josué Rodríguez <josuerdgz.ramirez@gmail.com>
2026-08-13 10:36:32 -0600

    Delete last audio_repair 2

--- backend/app/services/audio_repair 2/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Conservative, local repair of short digital audio dropouts."""
-
-from app.services.audio_repair.engine import (
-    AudioRepairResult,
-    DropoutIssue,
-    analyze_and_repair_wav,
-)
-
-__all__ = ["AudioRepairResult", "DropoutIssue", "analyze_and_repair_wav"]
